04942bf64599e7aa1fd503b9dbb6774c81cd15e9
jmmota <jean-marc.mota@thalesgroup.com>
2017-11-22 17:49:27 +0100

    Update uninitialized.c

--- examples/01-Uninitialized-access/uninitialized.c
+++ examples/01-Uninitialized-access/uninitialized.c
@@ -1,9 +1,12 @@
-#include "tis_builtins.h"
+//#include "tis_builtins.h"
 
-int foo (int* array )
+int foo (int* array , int n )
 {
+  /*@ requires n = 5 ;
+  ensures n >= 10;
+  */
   //int array[5] = {13, 7, 42, 0, 1};
-  int i, n=tis_interval (0,255);
+  int i; //, n= 5; //tis_interval (0,255);
 
   for (i = 0; i < 5; i++) {
     /* The variable 'n' is declared at line 4, but is never
@@ -20,7 +23,9 @@ int foo (int* array )
 
 int main () 
 {
+  int = 5;
+  
   int array [4] = {13, 7, 42, 0};
-  foo( array);
+  foo( array, n);
   return 0;
 }
